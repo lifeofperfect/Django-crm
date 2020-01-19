45e679e1143107f5d76a9db425f18aef16886111
lifeofatom <perfect.igbadumhe@gmail.com>
2020-01-19 18:31:21 +0000

    updated count views and templates

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_orders = orders.count()
+    delivered_orders = orders.filter(status='Delivered').count()
+    pending_orders = orders.filter(status='Pending').count()
+
     template_name = 'accounts/dashboard.html'
-    return render(request, template_name)
+    context = {
+        'orders':orders,
+        'customers':customers,
+        'total_orders':total_orders,
+        'delivered':delivered_orders,
+        'pending':pending_orders
+    }
+        
+    return render(request, template_name, context)
 
 def customer(request):
     template_name = 'accounts/customer.html'
     return render(request, template_name)
 
 def products(request):
+    products = Product.objects.all()
     template_name = 'accounts/products.html'
-    return render(request, template_name)
+    context = {
+        'products':products
+    }
+    return render(request, template_name, context)
 
